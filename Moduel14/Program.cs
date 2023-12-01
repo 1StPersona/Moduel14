52ca8881ff6718d2c29f151079f9ee09fe1d6113
Danila Zakharov <49668471+1StPersona@users.noreply.github.com>
2023-12-01 17:58:07 +0600

    Добавьте файлы проекта.

---
+++ Moduel14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moduel14
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game(2);
+            while (game.playersTurn())
+            {
+
+            }
+        }
+    }
+
+   
+
+   
+
+   
+   
+}
+
